c34aab7d6e10f424b44e536a3ceaf3f4c1890318
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2023-11-28 13:43:46 +0000

    Added listener-number buttons and dot. fixed BEM

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,42 @@
-import './main.scss'
+const numberButton = document.querySelectorAll<HTMLElement>(".buttons__number")
+const operationButton = document.querySelectorAll<HTMLElement>(".buttons__operator")
+const inputWindow = document.querySelector<HTMLElement>(".calculator__window")
+const equalsButton = document.querySelector(".buttons__operator--clear")
+const clearButton = document.querySelector<HTMLElement>(".buttons__operator--clear")
 
-const numberButtons = document.querySelectorAll<HTMLElement>(".button__number") 
-const operationButtons = document.querySelectorAll<HTMLElement>(".button__operator")
+if (!inputWindow || !equalsButton || !clearButton) {
+    throw new Error(`issue with a query selector`);
+  }
+
+//check linked  
+console.log(operationButton)
+
+// Defaults for before using the calc
+let inputNum = ' ';
+let haveDot = false;
+let result = null;
+
+
+// event listener for showing number buttons and dot on click
+
+numberButton.forEach(number => {
+    number.addEventListener("click", (event: Event) => {
+        if (event.target.innerText === '.' && !haveDot) {
+            haveDot = true;
+        } else if (event.target.innerText === '.' && haveDot)
+        return;
+
+    
+    inputNum += event.target.innerText;
+    inputWindow.innerText = inputNum;
+});
+});
+
+// event listener for operations - go back to this
+
+
+
+
+
+
+
