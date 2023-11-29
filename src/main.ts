b81900ccd07807dea74bc6e5bca267cfe60f971b
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2023-11-29 16:52:57 +0000

    fixed dot issue and some minor TS problems

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-// define buttons 
+// define buttons
 
 const numberButtons = document.querySelectorAll(".buttons__number");
 const operatorButtons = document.querySelectorAll(".buttons__operator");
@@ -7,60 +7,64 @@ const equalsButton = document.querySelector<HTMLElement>(".buttons__equals");
 const display = document.querySelector<HTMLElement>(".calculator__window");
 
 if (!clearButton || !equalsButton || !display) {
-    throw new Error("issue with a query selector");
-  }
+  throw new Error("issue with a query selector");
+}
 
 //defaults
-let firstInput= " ";
-let secondInput= " ";
-let operator= null;
-let haveDot = false;
-
+let firstInput = " ";
+let secondInput = " ";
+let operator = "";
 
 // handle number buttons click
 
 const handleNumberClick = (event: Event) => {
-    const clickedNumber = event.target.innerText;
+    const buttonHTML = event.currentTarget as HTMLButtonElement;
+    const clickedNumber = buttonHTML.innerText;
+    const isNotDot = clickedNumber !== ".";
+
     if (operator === "") {
-      firstInput += clickedNumber;
-      display.innerText = firstInput;
+      
+      if (!firstInput.includes(".") || isNotDot) {
+        firstInput += clickedNumber;
+        display.innerText = firstInput;
+      }
+     
     } else {
       secondInput += clickedNumber;
       display.innerText = secondInput;
     }
   };
 
-numberButtons.forEach((button) => {
-    button.addEventListener("click", handleNumberClick);
-  });
+numberButtons.forEach(button => {
+  button.addEventListener("click", handleNumberClick);
+});
 
+// handle allclear click
 
-// handle allclear click 
-const handleAcClick = (event: Event) => {
-    firstInput= "";
-    secondInput= "";
-    operator= "";
-    display.innerText="0"
-  };
+const handleAcClick = () => {
+  firstInput = "";
+  secondInput = "";
+  operator = "";
+  display.innerText = "0";
+};
 
-  clearButton.addEventListener("click", handleAcClick);
-
+clearButton.addEventListener("click", handleAcClick);
 
 // handle operation button clicks
 
 const handleOperationClick = (event: Event) => {
-    operator = event.target.innerText;
-    display.innerText = operator;
-  };
+      // YOU WILL NEED TO DO SOMETHING HERE TO REMOVE THE RED SQUIGGLY ;)
+  operator = event.target.innerText;
+  display.innerText = operator;
+};
 
-  operatorButtons.forEach((button) => {
-    button.addEventListener("click", handleOperationClick);
-  });
-  
+operatorButtons.forEach(button => {
+  button.addEventListener("click", handleOperationClick);
+});
 
-// handle equals and do the maths! (+,/,*,-) 
-const handleEqualsClick = (event: Event) => {
-    let result;
+// handle equals and do the maths! (+,/,*,-)
+const handleEqualsClick = () => {
+    let result:number;
     switch (operator) {
       case "+":
         result = parseFloat(firstInput) + parseFloat(secondInput);
@@ -78,22 +82,19 @@ const handleEqualsClick = (event: Event) => {
         result = parseFloat(firstInput) / parseFloat(secondInput);
         result = parseFloat(result.toFixed(5));
         break;
-    default:
-        console.log('Invalid operator');
+      default:
+        console.log("Invalid operator");
         break;
     }
-    display.innerText = result;
-    firstInput = result;
+        
+    display.innerText = String(result!);
+    firstInput = String(result!);
     secondInput = " ";
     operator = " ";
-    
   };
-  
-  equalsButton.addEventListener("click", handleEqualsClick);
 
+equalsButton.addEventListener("click", handleEqualsClick);
 
-
-// handle decimal point (limit it to 1 use)
 
 // handle 0 (limit it to 1 use)
 
